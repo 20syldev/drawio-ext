3862c4f4e8ef7d0da69fde6288c64988700922b8
Sylvain LAMBERT <slambert@zenetys.com>
2025-01-03 11:50:43 +0100

    MEDIUM: Added detailed comments about parameters, returns or throws

--- src/yana.js
+++ src/yana.js
@@ -26,6 +26,12 @@ Draw.loadPlugin(function (ui) {
 
     /**
      * Add custom menu items to the toolbar.
+     * 
+     * @param {string} label - The label of the menu item to be added.
+     * @param {string} title - The title or description of the menu item.
+     * @param {boolean} enabled - Flag to determine if the menu item is enabled or not.
+     * @param {Function} callback - The callback function to execute when the menu item is clicked.
+     * @param {HTMLElement} container - The container to add the menu item to.
      */
     toolbar.addMenuFunction('Select Base API', 'Select the base API to fetch data', true, () => selectBaseAPI(), toolbar.container);
     toolbar.addMenuFunction('Select Entity', 'Select entity and load graph', true, () => selectEntity(loadGraph), toolbar.container);
@@ -37,6 +43,8 @@ Draw.loadPlugin(function (ui) {
     /**
      * Opens a popup to enter a base API URL (must be HTTPS), validates it, and saves it as a custom property.
      * This function prompts the user to enter a base API URL.
+     * 
+     * @param {function} callback - A callback function to be called with the base API URL after it's validated and set.
      */
     function selectBaseAPI(callback) {
         const popup = new mxWindow('Select Base API', document.createElement('div'), 300, 300, 300, 120, true, true);
@@ -90,6 +98,8 @@ Draw.loadPlugin(function (ui) {
     /**
      * Select an entity from a list, load its related data, and save it as a custom property.
      * This function fetches a list of entities from the base API and allows the user to select one.
+     * 
+     * @param {function} callback - A callback function to be called after the entity is selected and saved.
      */
     function selectEntity(callback) {
         if (!baseAPI) {
@@ -146,6 +156,11 @@ Draw.loadPlugin(function (ui) {
      * Fetch data for devices and links related to the selected entity.
      * This function makes two API calls to retrieve device and link information.
      * It processes the data and returns it in a usable format.
+     * 
+     * @returns {Promise<{devices: Array, links: Array, deviceConnections: Object}>} - A promise that resolves to an object containing:
+     *    - devices: An array of devices related to the selected entity.
+     *    - links: An array of formatted links connecting devices.
+     *    - deviceConnections: An object mapping device IDs to the number of connections they have.
      */
     async function fetchData() {
         if (!baseAPI || !yanaEntity) {
@@ -216,6 +231,8 @@ Draw.loadPlugin(function (ui) {
      * Load the graph data based on the selected entity.
      * This function retrieves devices and links related to the selected entity 
      * and then constructs the graph elements (devices and links).
+     * 
+     * @throws {Error} If there is an error fetching or processing the graph data.
      */
     function loadGraph() {
         if (!yanaEntity || !baseAPI) return alert('Please select both a base API and an entity.');
@@ -236,6 +253,8 @@ Draw.loadPlugin(function (ui) {
      * Update the graph with the latest data from the selected entity.
      * This function fetches the latest devices and links, 
      * then updates the graph elements with the new data.
+     * 
+     * @throws {Error} If there is an error fetching or processing the graph data.
      */
     function updateGraph() {
         if (!yanaEntity || !baseAPI) return alert('Please select both a base API and an entity.');
@@ -259,6 +278,8 @@ Draw.loadPlugin(function (ui) {
     /**
      * Reset the graph (clear everything).
      * This function resets the graph, clearing any existing cells and entity selections.
+     * 
+     * @throws {Error} If there is an error while clearing the graph.
      */
     function resetGraph() {
         yanaEntity = null;
@@ -271,6 +292,13 @@ Draw.loadPlugin(function (ui) {
      * Create device elements in the graph.
      * This function creates device vertices for each device in the provided data.
      * It sets device names, IPs, and other properties based on the provided data.
+     * 
+     * @param {mxGraph} graph - The graph object where the devices will be created.
+     * @param {mxCell} parent - The parent cell to which the devices will be added.
+     * @param {Array} devices - The array of device data used to create the devices in the graph.
+     * @param {Object} [deviceConnections={}] - An optional object that maps device IDs to their connection count (default is empty).
+     * 
+     * @returns {Object} switchMap - A map where each key is a device ID and each value is the corresponding device vertex in the graph.
      */
     function createDevices(graph, parent, devices, deviceConnections = {}) {
         const switchMap = {};
@@ -306,6 +334,10 @@ Draw.loadPlugin(function (ui) {
      * Update device information in the graph.
      * This function updates the text and geometry of the device vertices 
      * based on the updated device data.
+     * 
+     * @param {mxGraph} graph - The graph object containing the devices to update.
+     * @param {Object} switchMap - A map where each key is a device ID and each value is the corresponding device vertex.
+     * @param {Array} devices - The array of updated device data used to update the devices in the graph.
      */
     function updateDevices(graph, switchMap, devices) {
         devices.forEach(device => {
@@ -322,6 +354,11 @@ Draw.loadPlugin(function (ui) {
      * Create link edges between devices in the graph.
      * This function creates edges between device vertices based on the link data provided.
      * It adds speed and duplex information to each link edge.
+     * 
+     * @param {mxGraph} graph - The graph object where the link edges will be created.
+     * @param {mxCell} parent - The parent cell to which the edges will be added.
+     * @param {Object} switchMap - A map where each key is a device ID and each value is the corresponding device vertex.
+     * @param {Array} links - An array of link data used to create edges between devices.
      */
     function createLinks(graph, parent, switchMap, links) {
         const processedLinks = new Set();
@@ -350,6 +387,11 @@ Draw.loadPlugin(function (ui) {
     /**
      * Update the links in the graph.
      * This function removes existing link edges and creates new edges based on updated link data.
+     * 
+     * @param {mxGraph} graph - The graph object where the links will be updated.
+     * @param {mxCell} parent - The parent cell to which the new links will be added.
+     * @param {Object} switchMap - A map where each key is a device ID and each value is the corresponding device vertex.
+     * @param {Array} links - An array of updated link data used to update the links between devices.
      */
     function updateLinks(graph, parent, switchMap, links) {
         const existingEdges = graph.getModel().getCells().filter(cell => graph.getModel().isEdge(cell));
@@ -361,6 +403,11 @@ Draw.loadPlugin(function (ui) {
      * Get the style for a link (edge) based on speed and duplex mode.
      * This function returns a string representing the style of the link 
      * based on the link's speed and duplex properties.
+     * 
+     * @param {number} speed - The speed of the link (in bits per second).
+     * @param {number} duplex - The duplex mode of the link (1 for half-duplex, 2 for full-duplex).
+     * 
+     * @returns {string} style - The style string to apply to the link (edge) based on speed and duplex.
      */
     function getLinkStyle(speed, duplex) {
         return duplex === 2 || speed < 1000000000
@@ -371,6 +418,12 @@ Draw.loadPlugin(function (ui) {
     /**
      * Add port labels to the edges (links) between devices.
      * This function adds labels to the edges to indicate the source and target ports for each link.
+     * 
+     * @param {mxGraph} graph - The graph object in which the edge labels will be added.
+     * @param {mxCell} edge - The edge (link) to which the labels will be added.
+     * @param {string} sourcePort - The source port label to display on the edge.
+     * @param {string} targetPort - The target port label to display on the edge.
+     * @param {string} linkKey - A unique key identifying the link (used to differentiate source and target labels).
      */
     function addPortLabels(graph, edge, sourcePort, targetPort, linkKey) {
         const existingSourceLabel = graph.getModel().getCell(`${linkKey}-source`);
@@ -405,6 +458,8 @@ Draw.loadPlugin(function (ui) {
      * Apply an organic layout to the graph.
      * This function arranges the graph using the organic layout algorithm 
      * to ensure that the devices and links are placed in a visually appealing way.
+     * 
+     * @param {mxGraph} graph - The graph object to which the organic layout will be applied.
      */
     function organicLayout(graph) {
         const layout = new mxFastOrganicLayout(graph);
